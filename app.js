df193589f80d86867c8021e24c48a8db872bd712
Brandon Thai <brandon88thai@hotmail.com>
2023-03-15 16:42:54 -0400

    Added require node-fetch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const port = 3000
 
 // const data = require('./data.json');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
