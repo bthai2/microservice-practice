3f7f957d0097860221c6a1f677b787aa87082f6d
Brandon Thai <brandon88thai@hotmail.com>
2023-03-15 18:00:41 -0400

    trying import node-fetch

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const port = 3000
 
 // const data = require('./data.json');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
